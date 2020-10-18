b8a2c7242f3397a9caa8bda85850cce4c3ce3063
HarperHao <m19834406344@163.com>
2020-10-18 09:30:33 +0800

    new

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+"""
+Author   : HarperHao
+TIME    ： 2020/10/17
+FUNCTION:  测试调试功能，set FLASK_DEBUG=1
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,8 +10,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello 638!'
 
 
-# if __name__ == '__main__':
-#     app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
